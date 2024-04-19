6c1819731e96898861085f814e8d2ae6010f3e46
Schukru <kilicsukru@hotmail.com>
2024-04-19 14:43:43 +0300

    updated import lines

--- src/test/java/_Typer/_TypeFromSpeech.java
+++ src/test/java/_Typer/_TypeFromSpeech.java
@@ -3,10 +3,7 @@ package _Typer;
 import Utils.Driver;
 import Utils.MyUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class _TypeFromSpeech {
 
